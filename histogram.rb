68ea2078f98789449cbeba124ac46e369b9b8862
David Mandel <MandelDL@gmail.com>
2015-02-09 14:50:43 -0500

    create hashes and iterate over them

--- histogram.rb
+++ histogram.rb
@@ -5,3 +5,25 @@ text = gets.chomp
 # Create an array out of the text variable
 words = text.split
 
+# Create a new hash, give it default value of 0
+frequencies = Hash.new(0)
+
+# iterate over words array: words.each
+# for each word we find, assume the word is a key in frequencies: {|word| frequencies[word]
+# and increment its value by one: +=1
+words.each {|word|
+    frequencies["#{word}"] +=1
+    }
+    
+# sort the frequencies array by word count:
+frequencies = frequencies.sort_by {|word, count|
+    count
+    }
+# Use reverse to reverse the sorted frequencies array:
+frequencies.reverse!
+
+# Iterate over each key/value pair in frequencies hash,
+# Then, puts each word, a single space, and its frequency to the console.
+frequencies.each {|key, value|
+puts "#{key} #{value}"
+}
